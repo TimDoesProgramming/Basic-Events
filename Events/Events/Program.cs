b0fbff1b6d09e53a5d65a4e7178c713d7f81e706
TimothyAlban <timothyalban@trentu.ca>
2022-01-24 21:20:21 -0500

    updated for a different type of event handler

--- Events/Events/Program.cs
+++ Events/Events/Program.cs
@@ -7,32 +7,42 @@ using System.Threading.Tasks;
 namespace Events
 {
     //this delegate will hold the event publisher
-    public delegate void eventDelegate();
-
+    public delegate void eventVoidDelegate();
+    //this delegate returns and string
+    public delegate string eventStringDelegate(string s);
     class Program
     {
-        //creates the publisher of the event
-        public static event eventDelegate publisher;
+        //creates an event for the void delegate
+        public static event eventVoidDelegate voidPublisher;
+        // creates an event publisher for the string delegate
+        public static event eventStringDelegate stringPublisher;
         static void Main(string[] args)
         {
             //the publisher takes functions to run
-            publisher += GoodBye;
-
+            voidPublisher += GoodBye;
+            stringPublisher += SayWhat;
             Hello();
-
             Console.ReadLine();
             
         }
+        //hello() is the function the activates the events
 
         public static void Hello()
         {
+            string s;
             Console.WriteLine("Hello");
-            //activates the event
-            publisher();
+
+            //activates the events
+            voidPublisher();
+            Console.WriteLine(stringPublisher("what"));
         }
         public static void GoodBye()
         {
             Console.WriteLine("Good bye");
+        }
+        public static string SayWhat(string s)
+        {
+            return s;
         }
 
         
